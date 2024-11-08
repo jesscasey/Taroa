f4917eed692d9546e0b2e55a39301f56a26c2068
Jess Casey <jessicacasey27@yahoo.com>
2024-11-08 20:57:09 +0000

    Add routes to tarot pages

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('main/home.html')
+
+@app.route("/directory")
+def directory():
+    return render_template('tarot/directory.html')
+
+@app.route("/reading")
+def reading():
+    return render_template('tarot/reading.html')
+
+if __name__ == '__main__':
+    app.run()
